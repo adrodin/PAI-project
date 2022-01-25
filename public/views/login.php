30079fc3f795316eb66b204a72f47bb52b7b9b1c
adrodin <adrian.dochnal@student.pk.edu.pl>
2022-01-25 17:47:56 +0100

    reformat views files

--- public/views/login.php
+++ public/views/login.php
@@ -2,21 +2,8 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>YerbaRate</title>
-    <!-- css-->
-    <link rel="stylesheet" type="text/css" href="public/css/style.css">
+    <?php include("head.php") ;?>
     <link rel="stylesheet" type="text/css" href="public/css/login.css">
-    <!-- fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-        rel="stylesheet">
-    <!-- icons -->
-    <script src="https://kit.fontawesome.com/86d7d3cd23.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -60,7 +47,7 @@
             </form>
             <div class="registration-text">
                 <p>Nie masz jeszcze konta? </p>
-                <a href="#"> Zarejestruj się </a>
+                <a href="registration"> Zarejestruj się </a>
             </div>
         </div>
     </div>
---
+++ public/views/navigation.php
@@ -0,0 +1,43 @@
+<nav>
+    <object class="logo-nav nav-item" type="image/svg+xml" data="public/img/logov2.svg">
+        Your browser does not support SVG.
+    </object>
+    <div class="nav-item">
+        <i class="fas fa-medal"></i>
+        <a href="#" class="nav-button">Ranking</a>
+    </div>
+    <div class="nav-item">
+        <i class="fas fa-medal"></i>
+        <a href="#" class="nav-button">Twoje oceny</a>
+    </div>
+    <div class="nav-item">
+        <i class="fas fa-plus"></i>
+        <a href="#" class="nav-button">Dodaj yerbe</a>
+    </div>
+    <div class="nav-item">
+        Szukaj
+    </div>
+
+    <div class="">
+        <div class="login-item">
+            <i class="fas fa-sign-in-alt"></i>
+            <a href="#" class="nav-button user-info">Zaloguj się</a>
+        </div>
+        <div class="login-text">
+            Nie masz konta? <a href="#">Zarejestruj się</a>
+        </div>
+    </div>
+    <div class="nav-item user-info">
+        <img class='nav-avatar' src="https://i.wpimg.pl/730x0/m.fotoblogia.pl/jajko-688cda13e203b23850b3998237.jpg">
+        Nick
+    </div>
+    <div class="nav-item">
+        <i class="fas fa-cog"></i>
+        <a href="#" class="nav-button">Ustawienia</a>
+    </div>
+    <div class="nav-item">
+        <i class="fas fa-sign-out-alt"></i>
+        <a href="#" class="nav-button">Wyloguj się</a>
+
+    </div>
+</nav>
